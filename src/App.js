08d02a636d2729bc7ba1264052e3a91ec7c450d2
mdblanchette <mdblanchette@ust.hk>
2023-08-02 11:17:52 +0800

    update initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import Button from "@mui/material/Button";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Button>hi</Button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
