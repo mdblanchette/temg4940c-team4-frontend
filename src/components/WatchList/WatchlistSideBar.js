e8548039095158b4166fcdc699169a316dc2e972
Ayush Kumar <ayushkumar2002hk@gmail.com>
2023-08-15 13:37:43 +0800

    Dummy Data added to "WatchlistSideBar"

--- src/components/WatchList/WatchlistSideBar.js
+++ src/components/WatchList/WatchlistSideBar.js
@@ -5,18 +5,18 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const dummyTableData = {
   0: [
-    { id: 1, name: 'Item 1', issuer: 'Issuer A', rating: 'A', migration: 'XX', allocation: 25 },
-    { id: 2, name: 'Item 2', issuer: 'Issuer B', rating: 'B', migration: 'XX', allocation: 15 },
-    { id: 3, name: 'Item 3', issuer: 'Issuer C', rating: 'C', migration: 'XX', allocation: 30 },
-    { id: 4, name: 'Item 4', issuer: 'Issuer D', rating: 'C', migration: 'XX', allocation: 10 },
+    { id: 1, name: 'Item 1', issuer: 'Issuer A', rating: 'A', migration: 'XX', allocation: 25, country: "Canada", spread: "12%", coupons: "$100", maturityDate: "12/07/2023", liquidity: "$1,000,000", outstandingAmount: "$10,000", bid: "12%", ask: "15%", netChange: "3%"},
+    { id: 2, name: 'Item 2', issuer: 'Issuer B', rating: 'B', migration: 'XX', allocation: 15, country: "United States", spread: "15%", coupons: "$900", maturityDate: "12/06/2024", liquidity: "$2,000,000", outstandingAmount: "$20,000", bid: "13%", ask: "16%", netChange: "4%"},
+    { id: 3, name: 'Item 3', issuer: 'Issuer C', rating: 'C', migration: 'XX', allocation: 30, country: "Japan", spread: "12%", coupons: "$500", maturityDate: "12/07/2025", liquidity: "$3,000,000", outstandingAmount: "$30,000", bid: "14%", ask: "17%", netChange: "5%" },
+    { id: 4, name: 'Item 4', issuer: 'Issuer D', rating: 'C', migration: 'XX', allocation: 10, country: "Korea", spread: "11%", coupons: "$400", maturityDate: "12/07/2026", liquidity: "$4,000,000", outstandingAmount: "$40,000", bid: "15%", ask: "18%", netChange: "6%"},
   ],
   1: [
-    { id: 5, name: 'Item 5', issuer: 'Issuer E', rating: 'C', migration: 'XX', allocation: 18 },
-    { id: 6, name: 'Item 6', issuer: 'Issuer C', rating: 'A', migration: 'XX', allocation: 27 },
+    { id: 5, name: 'Item 5', issuer: 'Issuer E', rating: 'C', migration: 'XX', allocation: 18, country: "Korea", spread: "11%", coupons: "$400", maturityDate: "12/09/2026", liquidity: "$5,000,000", outstandingAmount: "$50,000", bid: "16%", ask: "19%", netChange: "7%"},
+    { id: 6, name: 'Item 6', issuer: 'Issuer C', rating: 'A', migration: 'XX', allocation: 27, country: "China", spread: "15%", coupons: "$700", maturityDate: "12/10/2026", liquidity: "$6,000,000", outstandingAmount: "$60,000", bid: "17%", ask: "20%", netChange: "8%"},
     // Add more data as needed for Portfolio 2
   ],
   2: [
-    { id: 7, name: 'Item 7', issuer: 'Issuer F', rating: 'A', migration: 'XX', allocation: 40 },
+    { id: 7, name: 'Item 7', issuer: 'Issuer F', rating: 'A', migration: 'XX', allocation: 40, country: "Japan", spread: "14%", coupons: "$500", maturityDate: "12/08/2027", liquidity: "$7,000,000", outstandingAmount: "$70,000", bid: "18%", ask: "21%", netChange: "9%"},
     // Add more data as needed for Portfolio 3
   ],
 };
